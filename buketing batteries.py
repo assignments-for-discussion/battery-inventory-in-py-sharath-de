749ca4c306e3ae98ca59fe7efb623ad879b1523a
Sharath BR <68052307+sharath-de@users.noreply.github.com>
2022-01-21 18:56:39 +0530

    Add files via upload

---
+++ buketing batteries.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+def count(a):
+    low=[]
+    med=[]
+    high=[]
+    for i in range(len(a)):
+        if a[i]<400:
+            low.append(a[i])
+        elif a[i]>400 and a[i]<919:
+            med.append(a[i])
+        else:
+            high.append(a[i])
+    l=[low,len(low)]
+    m=[med,len(med)]
+    h=[high,len(high)]
+    return l,m,h
+a=[100, 300, 500, 600, 900, 1000]
+print(count(a))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
